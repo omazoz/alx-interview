9678189565c205b77e52e4b3f5ac90599efbb3f5
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2024-03-02 20:16:12 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,19 +3,12 @@
 
 
 def makeChange(coins, total):
-    """ Generate changes needed to reach total
-    """
-    if total <= 0:
-        return 0
-    check = 0
-    temp = 0
-    coins.sort(reverse=True)
-    for i in coins:
-        while check < total:
-            check += i
-            temp += 1
-        if check == total:
-            return temp
-        check -= i
-        temp -= 1
-    return -1
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return -1 if dp[total] == total + 1 else dp[total]
