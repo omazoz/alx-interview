75168d77c4dce1b50cb3f6f2d4feea168187a5db
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2024-03-02 20:19:22 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,12 +3,24 @@
 
 
 def makeChange(coins, total):
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    """ Generate changes needed to reach total
+    """
+    memo = dict()
 
-    for amount in range(1, total + 1):
+    def dp(n):
+        # return memo if n is in memo
+        if n in memo: return memo[n]
+        # return -1 if no combination can sum to n
+        if n < 0: return -1
+        # return 0 if n is zero
+        if n == 0: return 0
+        # initialize minimum number of coins needed to infinity
+        res = float('inf')
         for coin in coins:
-            if coin <= amount:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            subproblem = dp(n - coin)
+            if subproblem != -1: res = min(res, 1 + subproblem)
+        # store the result into memo
+        memo[n] = res if res != float('inf') else -1
+        return memo[n]
 
-    return -1 if dp[total] == total + 1 else dp[total]
+    return dp(total)
